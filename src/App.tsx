c51caafd7d0200f31543d66103918ab7d4709648
PtCookie <ptcookie@outlook.com>
2021-03-06 16:56:05 +0900

    feat: Clean up asset
    Remove Create React App assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
 function App(): React.ReactElement {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <>
+      <header className='flex justify-center'>
         <p className='text-xl'>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello <code>React</code>!
         </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
       </header>
-    </div>
+    </>
   );
 }
 
